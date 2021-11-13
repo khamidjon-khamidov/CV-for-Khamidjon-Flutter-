2114ccf827cefb0145dc4946edd9f27dc82b0c84
Khamidjon <hamidovhamid1998@gmail.com>
2021-11-13 13:44:11 +0500

    init pages for other details

---
+++ lib/routes.dart
@@ -0,0 +1,9 @@
+class Routes {
+  static const String home = '/home';
+  static const String aboutMePage = '/aboutMePage';
+  static const String skills = '/skills';
+  static const String achievements = '/achievements';
+  static const String projects = '/projects';
+  static const String posts = '/posts';
+  static const String appDetails = '/appDetails';
+}
---
+++ lib/ui/screens/app_details/view/app_details.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppDetails extends StatelessWidget {
+  const AppDetails({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
