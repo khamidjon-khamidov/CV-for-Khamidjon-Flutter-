161dec162a5309da8983ce42966d239a28783bb9
Khamidjon <hamidovhamid1998@gmail.com>
2021-11-21 07:02:04 +0500

    add project.g.dart

---
+++ lib/domain/models/projects/project.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'project.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Project _$ProjectFromJson(Map<String, dynamic> json) => Project(
+      id: json['projectId'] as int,
+      time: json['projectTime'] as String,
+      title: json['projectTitle'] as String,
+      description: json['projectDes'] as String,
+      link: json['projectLink'] as String,
+      gitLink: json['projectGitLink'] as String,
+    );
+
+Map<String, dynamic> _$ProjectToJson(Project instance) => <String, dynamic>{
+      'projectId': instance.id,
+      'projectTime': instance.time,
+      'projectTitle': instance.title,
+      'projectDes': instance.description,
+      'projectLink': instance.link,
+      'projectGitLink': instance.gitLink,
+    };
