19710a20f8b141afabc20c7f5c5434bfe9150a14
Khamidjon <hamidovhamid1998@gmail.com>
2021-12-03 11:23:51 +0500

    add menu buttons

--- lib/ui/screens/achievements/achievements_page.dart
+++ lib/ui/screens/achievements/achievements_page.dart
@@ -2,15 +2,18 @@ library achievements_page;
 
 import 'package:bloc/bloc.dart';
 import 'package:bloc_concurrency/bloc_concurrency.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cv_for_khamidjon/domain/models/achievements/achievement_group.dart';
 import 'package:cv_for_khamidjon/domain/models/bloc_response.dart';
 import 'package:cv_for_khamidjon/domain/repositories/achievements_repository.dart';
-import 'package:cv_for_khamidjon/generated/l10n.dart';
 import 'package:cv_for_khamidjon/ui/components/app_drawer.dart';
+import 'package:cv_for_khamidjon/ui/components/snackbar.dart';
 import 'package:cv_for_khamidjon/ui/theme/color_extensions.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 part 'bloc/achievements_bloc.dart';
 part 'bloc/achievements_event.dart';
--- lib/ui/screens/achievements/view/achievements.dart
+++ lib/ui/screens/achievements/view/achievements.dart
@@ -9,6 +9,29 @@ class AchievementsPage extends StatefulWidget {
 
 class _AchievementsPageState extends State<AchievementsPage> {
   int _selectedIndex = 0;
+  RefreshController _refreshController = RefreshController(initialRefresh: false);
+
+  void _onRefresh() async =>
+      context.read<AchievementsBloc>().add(_GetAchievementGroupEvent(_selectedIndex));
+
+  @override
+  void initState() {
+    context.read<AchievementsBloc>().stream.listen((state) {
+      if (state is _AchievementsErrorState) {
+        AppSnackBar.showError(
+          ScaffoldMessenger.of(context),
+          iconData: Icons.done,
+          title: state.extraMessage,
+        );
+      } else if (state.extraMessage != null) {
+        AppSnackBar.showInfo(
+          ScaffoldMessenger.of(context),
+          title: state.extraMessage!,
+        );
+      }
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,28 +56,79 @@ class _AchievementsPageState extends State<AchievementsPage> {
             title: Text(state.achievementGroup.title),
           ),
           drawer: AppDrawer(),
-          body: Container(
-            child: Text(state.achievementGroup.title),
+          body: SmartRefresher(
+            enablePullDown: true,
+            controller: _refreshController,
+            onRefresh: _onRefresh,
+            child: ListView.builder(
+              physics: BouncingScrollPhysics(),
+              itemCount: state.achievementGroup.achievements.length,
+              itemBuilder: (c, i) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 8.w,
+                    vertical: 4.h,
+                  ),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0.r),
+                      side: BorderSide(
+                        color: Theme.of(context).colorScheme.secondary,
+                        width: 2.0.w,
+                      ),
+                    ),
+                    elevation: 15.r,
+                    child: Column(
+                      children: [
+                        CachedNetworkImage(
+                          imageUrl: state.achievementGroup.achievements[i].imageLink,
+                          // imageBuilder: (context, imageProvider) => Container(
+                          //   width: width,
+                          //   height: 600,
+                          //   decoration: BoxDecoration(
+                          //     borderRadius: BorderRadius.circular(12.r),
+                          //     shape: BoxShape.rectangle,
+                          //     image: DecorationImage(
+                          //       image: imageProvider,
+                          //       fit: BoxFit.cover,
+                          //     ),
+                          //   ),
+                          // ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
           bottomNavigationBar: BottomNavigationBar(
-            items: state.menus
-                .map(
-                  (e) => BottomNavigationBarItem(
-                    icon: Icon(Icons.done),
-                    label: e,
-                  ),
-                )
-                .toList(),
-            currentIndex: state.index,
-            unselectedItemColor: Theme.of(context).colorScheme.disabledTextDark,
-            selectedItemColor: Theme.of(context).colorScheme.secondary,
-            onTap: (int index) {
-              print('Khamidjon: tapped bottom: $index');
-              context.read<AchievementsBloc>().add(_GetAchievementGroupEvent(index));
-            },
-          ),
+              items: state.menus
+                  .map(
+                    (e) => BottomNavigationBarItem(
+                      icon: Icon(getBottomSheetIcon(e)),
+                      label: e,
+                    ),
+                  )
+                  .toList(),
+              currentIndex: state.index,
+              unselectedItemColor: Theme.of(context).colorScheme.disabledTextDark,
+              selectedItemColor: Theme.of(context).colorScheme.secondary,
+              onTap: (int index) {
+                _selectedIndex = index;
+                context.read<AchievementsBloc>().add(_GetAchievementGroupEvent(index));
+              }),
         );
       },
     );
+  }
+
+  IconData getBottomSheetIcon(String menu) {
+    if (menu.toLowerCase().contains('android')) {
+      return Icons.android;
+    } else if (menu.toLowerCase().contains('algorithms')) {
+      return Icons.code;
+    }
+    return Icons.menu;
   }
 }
